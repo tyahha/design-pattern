1451822b039c184ad1c7a467c0c4f8d62d17d0f5
tyahhahha <tyahhahha@gmail.com>
2018-05-25 14:19:40 +0900

    bridgeパターンでの機能の種類を１つ増やした

--- bridge/src/main/java/Main.java
+++ bridge/src/main/java/Main.java
@@ -3,9 +3,12 @@ public class Main {
         Display d1 = new Display(new StringDisplayImpl("Hello, Japan."));
         Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
         CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+        RandomCountDisplay d4 = new RandomCountDisplay(new StringDisplayImpl("Hello, aaaaaaaaaaaaa."));
         d1.display();
         d2.display();
         d3.display();
         d3.multiDisplay(5);
+        d4.display();
+        d4.randomDisplay(10);
     }
 }
